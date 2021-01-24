3fc6264bdab2aa2bfe31f7a1fe9d6a8719ce086a
Necroman33 <57381978+Necroman33@users.noreply.github.com>
2021-01-24 13:18:57 +0300

    1st commit

---
+++ cross-zero_game/cross-zero_game/Game.xaml.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace cross_zero_game
+{
+    /// <summary>
+    /// Логика взаимодействия для Game.xaml
+    /// </summary>
+    public partial class Game : Window
+    {
+        CrossZeroEntities context = new CrossZeroEntities();
+        Logic logic = new Logic();
+        char[,] arr = new char[3, 3];
+        char symbol_u = 'x';
+        char symbol_i = 'o';
+        char first = 'u';
+        public Game(string user)
+        {
+            InitializeComponent();
+            username_label.Content = user;
+            var scores = context.Scores.ToList();
+            var user_scores = scores.Where(u => u.Username == user).FirstOrDefault();
+            if (user != "Неизвестный")
+            {
+                Wins.Content = user_scores.Wins;
+                Loses.Content = user_scores.Defeats;
+                Draws.Content = user_scores.Draws;
+            }
+            else
+            {
+                Wins.Content = 0;
+                Loses.Content = 0;
+                Draws.Content = 0;
+            }
+            restart();
+        }
+
+        private void click(Button clicked)
+        {
+            clicked.Content = symbol_u;
+            clicked.IsEnabled = false;
+
+            string ii_turn = logic.turn(arr,symbol_u,symbol_i,first);
+
+            int x = Convert.ToInt32(ii_turn.Substring(4, 1));
+            int y = Convert.ToInt32(ii_turn.Substring(6, 1));
+
+            arr[x-1,y-1] = symbol_i;
+
+            Button curr_turn = (Button)this.FindName(ii_turn);
+
+            curr_turn.Content = symbol_i;
+            curr_turn.IsEnabled = false;
+
+            result();
+        }
+
+        private void result()
+        {
+            string result = logic.result(arr, symbol_u, symbol_i);
+            string user = username_label.Content.ToString();
+            if (result != "")
+            {
+                switch (result)
+                {
+                    case "Win":
+                        block_game();
+                        if (user != "Неизвестный")
+                        {
+                            logic.addPoint(user, result);
+                        }
+                        Wins.Content = Convert.ToInt32(Wins.Content)+1;
+                        MessageBox.Show("Не знаю как, но вы выиграли, скорее всего это моя ошибка)))) ну или вы изменили код чтобы проверить работоспособность функции :)", "Результат игры");
+                        break;
+                    case "Lose":
+                        block_game();
+                        if (user != "Неизвестный")
+                        {
+                            logic.addPoint(user, result);
+                        }
+                        Loses.Content = Convert.ToInt32(Loses.Content) + 1;
+                        MessageBox.Show("Вы проиграли. Если хотите, можете попробовать снова :)", "Результат игры");
+                        break;
+                    case "Draw":
+                        block_game();
+                        if (user != "Неизвестный")
+                        {
+                            logic.addPoint(user, result);
+                        }
+                        Draws.Content = Convert.ToInt32(Draws.Content) + 1;
+                        MessageBox.Show("Ничья, на врятли вы сможете больше:) Если хотите, можете попробовать снова.", "Результат игры");
+                        break;
+                }
+            }
+        }
+
+        private void block_game()
+        {
+            for (var x_ = 0; x_ < 3; x_++)
+            {
+                for (var y_ = 0; y_ < 3; y_++)
+                {
+                    Button curr_butn = (Button)this.FindName($"cell{x_ + 1}_{y_ + 1}");
+                    curr_butn.IsEnabled = false;
+                }
+            }
+        }
+
+        private void restart()
+        {
+            if (crossBtn.IsChecked == true)
+            {
+                symbol_u = 'x';
+                symbol_i = 'o';
+            }
+            else
+            {
+                symbol_u = 'o';
+                symbol_i = 'x';
+            }
+            if (firstBtn.IsChecked == true)
+            {
+                first = 'u';
+            }
+            else
+            {
+                first = 'i';
+            }
+            for (var x = 0; x < 3; x++)
+            {
+                for (var y = 0; y < 3; y++)
+                {
+                    Button curr_butn = (Button)this.FindName($"cell{x + 1}_{y + 1}");
+                    curr_butn.IsEnabled = true;
+                    curr_butn.Content = "";
+                    arr[x, y] = '0';
+                }
+            }
+            if (first == 'i')
+            {
+                arr[1, 1] = symbol_i;
+                cell2_2.Content = symbol_i;
+                cell2_2.IsEnabled = false;
+            }
+        }
+
+        private void Cell1_1_Click(object sender, RoutedEventArgs e)
+        {
+            arr[0, 0] = symbol_u;
+            click(cell1_1);
+        }
+
+        private void Cell1_2_Click(object sender, RoutedEventArgs e)
+        {
+            arr[0, 1] = symbol_u;
+            click(cell1_2);
+        }
+
+        private void Cell1_3_Click(object sender, RoutedEventArgs e)
+        {
+            arr[0, 2] = symbol_u;
+            click(cell1_3);
+        }
+
+        private void Cell2_1_Click(object sender, RoutedEventArgs e)
+        {
+            arr[1, 0] = symbol_u;
+            click(cell2_1);
+        }
+
+        private void Cell2_2_Click(object sender, RoutedEventArgs e)
+        {
+            arr[1, 1] = symbol_u;
+            click(cell2_2);
+        }
+
+        private void Cell2_3_Click(object sender, RoutedEventArgs e)
+        {
+            arr[1, 2] = symbol_u;
+            click(cell2_3);
+        }
+
+        private void Cell3_1_Click(object sender, RoutedEventArgs e)
+        {
+            arr[2, 0] = symbol_u;
+            click(cell3_1);
+        }
+
+        private void Cell3_2_Click(object sender, RoutedEventArgs e)
+        {
+            arr[2, 1] = symbol_u;
+            click(cell3_2);
+        }
+
+        private void Cell3_3_Click(object sender, RoutedEventArgs e)
+        {
+            arr[2, 2] = symbol_u;
+            click(cell3_3);
+        }
+
+        private void Reset_btn_Click(object sender, RoutedEventArgs e)
+        {
+            restart();
+        }
+
+        private void To_main_btn_Click(object sender, RoutedEventArgs e)
+        {
+            string user = username_label.Content.ToString();
+            if (user != "Неизвестный")
+            {
+                User_menu use = new User_menu(user);
+                this.Hide();
+                use.ShowDialog();
+            }
+            else
+            {
+                MainWindow mainw = new MainWindow();
+                this.Hide();
+                mainw.ShowDialog();
+            }
+        }
+    }
+}
---
+++ cross-zero_game/cross-zero_game/Logic.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cross_zero_game
+{
+    class Logic
+    {
+        CrossZeroEntities context = new CrossZeroEntities();
+        private bool check_turn(char[,] arr, char user, char ii, char first)
+        {
+            int user_symbols = 0;
+            int ii_symbols = 0;
+            for (var x = 0; x < 3; x++)
+            {
+                for (var y = 0; y < 3; y++)
+                {
+                    if(arr[x,y] == user)
+                    {
+                        user_symbols++;
+                    }
+                    else if(arr[x,y] == ii)
+                    {
+                        ii_symbols++;
+                    }
+                }
+            }
+            if (user_symbols > ii_symbols && first =='u')
+            {
+                return true;
+            }
+            else
+            {
+                if (user_symbols < ii_symbols && first == 'u')
+                {
+                    return false;
+                }
+                else
+                {
+                    if (user_symbols == ii_symbols && first == 'i')
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        public string turn(char[,] arr, char user, char ii, char first)
+        {
+            string result = "";
+            if(check_turn(arr, user, ii, first))
+            {
+                result+=important_turn(arr, ii, ii);
+                if (result != "")
+                {
+                    int x = Convert.ToInt32(result.Substring(4, 1));
+                    int y = Convert.ToInt32(result.Substring(6, 1));
+                    arr[x - 1, y - 1] = ii;
+                }
+            }
+            if (check_turn(arr, user, ii, first))
+            {
+                result+=important_turn(arr, user, ii);
+                if (result != "")
+                {
+                    int x = Convert.ToInt32(result.Substring(4, 1));
+                    int y = Convert.ToInt32(result.Substring(6, 1));
+                    arr[x - 1, y - 1] = ii;
+                }
+            }
+            if (check_turn(arr, user, ii, first))
+            {
+                result += logic_turn(arr, user, ii, first);
+            }
+            return result;
+        }
+
+        private string important_turn(char[,] arr, char line_symbol, char ii)
+        {
+            char x = line_symbol;
+            if ((((arr[0,0] == x) && (arr[1,0] == x)) || ((arr[0,2] == x) && (arr[1,1] == x)) || ((arr[2,1] == x) && (arr[2,2] == x))) && (arr[2,0] == '0'))
+            {
+                return "cell3_1";
+            }
+            else
+            {
+                if ((((arr[0,1] == x) && (arr[1,1] == x)) || ((arr[2,0] == x) && (arr[2,2] == x))) && (arr[2,1] == '0'))
+                { return "cell3_2"; }
+                else
+                {
+                    if ((((arr[0,0] == x) && (arr[1,1] == x)) || ((arr[0,2] == x) && (arr[1,2] == x)) || ((arr[2,0] == x) && (arr[2,1] == x))) && (arr[2,2] == '0'))
+                    { return "cell3_3"; }
+                    else
+                    {
+                        if ((((arr[1,0] == x) && (arr[1,1] == x)) || ((arr[0,2] == x) && (arr[2,2] == x))) && (arr[1,2] == '0'))
+                        { return "cell2_3"; }
+                        else
+                        {
+                            if ((((arr[0,0] == x) && (arr[0,1] == x)) || ((arr[1,1] == x) && (arr[2,0] == x)) || ((arr[1,2] == x) && (arr[2,2] == x))) && (arr[0,2] == '0'))
+                            { return "cell1_3"; }
+                            else
+                            {
+                                if ((((arr[0,0] == x) && (arr[0,2] == x)) || ((arr[1,1] == x) && (arr[2,1] == x))) && (arr[0,1] == '0'))
+                                { return "cell1_2"; }
+                                else
+                                {
+                                    if ((((arr[0,1] == x) && (arr[0,2] == x)) || ((arr[1,0] == x) && (arr[2,0] == x)) || ((arr[1,1] == x) && (arr[2,2] == x))) && (arr[0,0] == '0'))
+                                    { return "cell1_1"; }
+                                    else
+                                    {
+                                        if ((((arr[0,0] == x) && (arr[2,0] == x)) || ((arr[1,1] == x) && (arr[1,2] == x))) && (arr[1,0] == '0'))
+                                        { return "cell2_1"; }
+                                        else
+                                        {
+                                            if ((((arr[0,0] == x) && (arr[2,2] == x)) || ((arr[2,0] == x) && (arr[0,2] == x)) || ((arr[0,1] == x) && (arr[2,1] == x)) || ((arr[1,0] == x) && (arr[1,2] == x))) && (arr[1,1] == '0'))
+                                            { return "cell2_2"; }
+                                            else
+                                            {
+                                                return "";
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private string logic_turn(char[,] arr, char user, char ii, char first)
+        {
+            int fist_sybols = 0;
+            for (var x = 0; x < 3; x++)
+            {
+                for (var y = 0; y < 3; y++)
+                {
+                    if (first =='u')
+                    {
+                        if (arr[x,y] == user)
+                        {
+                            fist_sybols++;
+                        }
+                    }
+                    else if (first =='i')
+                    {
+                        if (arr[x, y] == ii)
+                        {
+                            fist_sybols++;
+                        }
+                    }
+                }
+            }
+            switch (fist_sybols)
+            {
+                case 1:
+                    if (arr[1,1] == '0')
+                    {
+                        return "cell2_2";
+                    }
+                    else
+                    {
+                        if (arr[2, 0] == '0')
+                        {
+                            return "cell3_1";
+                        }
+                        else
+                        {
+                            return "cell1_3";
+                        }
+                    }
+                case 2:
+                    if ((arr[1, 1] == ii) && (((arr[0, 1] == user) && (arr[2, 2] == user)) || ((arr[0, 1] == user) && (arr[2, 0] == user))) && (arr[1, 2] == '0'))
+                    {
+                        return "cell2_3";
+                    }
+                    else
+                    {
+                        if ((arr[1, 1] == ii) && ((arr[0, 0] == user) || (arr[2, 2] == user) || (arr[0, 2] == user) || (arr[2, 0] == user)) && (arr[2, 1] == '0'))
+                        {
+                            return "cell3_2";
+                        }
+                        else
+                        {
+                            if ((arr[1, 1] == ii) && (arr[1, 0] == user) && (arr[2, 0] == '0'))
+                            {
+                                return "cell3_1";
+                            }
+                            else
+                            {
+                                if ((arr[1, 1] == ii) && (arr[1, 2] == user) && (arr[0, 2] == '0'))
+                                {
+                                    return "cell1_3";
+                                }
+                                else
+                                {
+                                    if ((arr[1, 1] == ii) && (arr[2, 1] == user) && (arr[2, 2] == '0'))
+                                    {
+                                        return "cell3_3";
+                                    }
+                                    else
+                                    {
+                                        if ((arr[0, 1] == ii) && (arr[1, 0] == user) && (arr[0, 0] == '0'))
+                                        {
+                                            return "cell1_1";
+                                        }
+                                        else
+                                        {
+                                            if ((arr[2, 0] == ii) && (arr[1, 1] == user) && (arr[0, 0] == '0'))
+                                            {
+                                                return "cell1_1";
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
+                case 3:
+                    if ((arr[1,1] == ii) && ((arr[1,0] == user) && (arr[0,1] == user)) && (arr[0,0] == '0'))
+                    {
+                        return "cell1_1";
+                    }
+                    else
+                    {
+                        if ((arr[1,1] == ii) && ((arr[1,0] == user) && (arr[2,1] == user)) && (arr[2,0] == '0'))
+                        {
+                            return "cell3_1";
+                        }
+                        else
+                        {
+                            if ((arr[2,1] == ii) && ((arr[1,2] == user) && (arr[0,1] == user)) && (arr[2,2] == '0'))
+                            {
+                                return "cell3_3";
+                            }
+                            else
+                            {
+                                if ((arr[1,1] == ii) && ((arr[1,2] == user) && (arr[0,1] == user)) && (arr[0,2] == '0'))
+                                {
+                                    return "cell1_3";
+                                }
+                                else
+                                {
+                                    for (var y = 0; y < 3; y++)
+                                    {
+                                        for (var x = 0; x < 3; x++)
+                                        {
+                                            if (arr[y,x] != user && arr[y, x] != ii)
+                                            {
+                                                return $"cell{y+1}_{x+1}";
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
+                case 4: 
+                    for (var y = 0; y < 3; y++)
+                    {
+                        for (var x = 0; x < 3; x++)
+                        {
+                            if (arr[y, x] != user && arr[y, x] != ii)
+                            {
+                                return $"cell{y+1}_{x+1}";
+                            }
+                        }
+                    }
+                    break;
+                case 5:
+                    for (var y = 0; y < 3; y++)
+                    {
+                        for (var x = 0; x < 3; x++)
+                        {
+                            if (first == 'u')
+                            {
+                                if (arr[y, x] != user)
+                                {
+                                    return $"cell{y + 1}_{x + 1}";
+                                }
+                            }
+                            else
+                            {
+                                if (arr[y, x] != user && arr[y, x] != ii)
+                                {
+                                    return $"cell{y + 1}_{x + 1}";
+                                }
+                            }
+                        }
+                    }
+                    break;
+
+            }
+            return "";
+        }
+
+        private bool combo(char[,] arr, char x)
+        {
+            if (((arr[0,0] == x) && (arr[1,0] == x) && (arr[2,0] == x)) || ((arr[0,1] == x) && (arr[1,1] == x) && (arr[2,1] == x)) || 
+                ((arr[0,2] == x) && (arr[1,2] == x) && (arr[2,2] == x)) || ((arr[0,0] == x) && (arr[0,1] == x) && (arr[0,2] == x)) || 
+                ((arr[1,0] == x) && (arr[1,1] == x) && (arr[1,2] == x)) || ((arr[2,0] == x) && (arr[2,1] == x) && (arr[2,2] == x)) || 
+                ((arr[0,0] == x) && (arr[1,1] == x) && (arr[2,2] == x)) || ((arr[2,0] == x) && (arr[1,1] == x) && (arr[0,2] == x)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+    }
+        public string result(char[,] arr, char user, char ii)
+        {
+            if (combo(arr, user))
+            {
+                return "Win";
+            }
+            else
+            {
+                if (combo(arr, ii))
+                {
+                    return "Lose";
+                }
+                else
+                {
+                    int check = 0;
+                    for (var y = 0; y < 3; y++)
+                    {
+                        for (var x = 0; x < 3; x++)
+                        {
+                            if (arr[y, x] != user && arr[y, x] != ii)
+                            {
+                                check++;
+                            }
+                        }
+                    }
+                    if (check == 0)
+                    {
+                        return "Draw";
+                    }
+                    else
+                    {
+                        return "";
+                    }
+                }
+            }
+        }
+
+        public void addPoint(string user, string pointType)
+        {
+            var scores = context.Scores.ToList();
+            var user_scores = scores.Where(u => u.Username == user).FirstOrDefault();
+            switch (pointType)
+            {
+                case "Win":
+                    user_scores.Wins++;
+                    break;
+                case "Lose":
+                    user_scores.Defeats++;
+                    break;
+                case "Draw":
+                    user_scores.Draws++;
+                    break;
+            }
+            context.SaveChanges();
+        }
+    }
+}
